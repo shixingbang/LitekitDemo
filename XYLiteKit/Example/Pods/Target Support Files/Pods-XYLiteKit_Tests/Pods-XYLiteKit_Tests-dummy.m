6f83bc5e83bb2bd0844bf998fdb21ad9844a7e60
renhao <renhao@xiaohongshu.com>
2021-04-15 09:51:05 +0800

    333

--- XYLiteKit/Example/Pods/Target Support Files/Pods-XYLiteKit_Tests/Pods-XYLiteKit_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XYLiteKit_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_XYLiteKit_Tests
-@end
--- XYLiteKit/Example/Pods/Target Support Files/XYLiteKit/XYLiteKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XYLiteKit : NSObject
-@end
-@implementation PodsDummy_XYLiteKit
-@end
